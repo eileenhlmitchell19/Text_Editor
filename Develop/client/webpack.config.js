a4139187231060834925b1a1f7957d74daabcbd0
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-03-28 22:16:23 -0700

    no more errors

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+//const path = require('path');
 
 // TODO: Add and configure workbox plugins for a service workerDONE and manifest file.
 // TODO: Add CSS loaders and babel to webpack.DONE
@@ -11,7 +13,8 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+
     },
     output: {
       filename: '[name].bundle.js',
@@ -24,23 +27,24 @@ module.exports = () => {
     //   // The `hot` option is to use the webpack-dev-server in combination with the hot module replacement API.
     //   hot: 'only',
     // },
-    // plugins: [
+    plugins: [
     //   new HtmlWebpackPlugin({
     //     template: './index.html',
     //     title: 'Webpack Plugin',
     //   }),
-    //   new InjectManifest({
-    //     swSrc: './client/src/src-sw.js',
-    //     swDest: 'service-worker.js',
-    //   }), 
-    // ],
+      // new InjectManifest({
+      //   swSrc: './src/src-sw.js',
+      //   swDest: 'service-worker.js',
+      // }), 
+      new MiniCssExtractPlugin() 
+    ],
 
     module: {
       rules: [
-        // {
-        //   test: /\.css$/i,
-        //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        // },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: 'asset/resource',
